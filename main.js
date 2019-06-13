aae68c9e22ba60adc77e77440f2b6d5e04dc4bc9
axecopfire <sbraun990@gmail.com>
2019-06-13 16:00:14 -0400

    able to get student name

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
+var loadStudentStyle = function (event) {
 
-var styleSwapper = function () {
+    var studentName = event.target.classList;
 
-    var createStudentElement = function (studentName) {
-        var studentItem = document.createElement("li");
-        studentItem.classList.add("style-swap__item style-swap__item--" + studentName);
-        studentItem.textContent = studentName;
+    studentName.forEach(function(string) {
+        if(string.indexOf("--") !== -1) {
+            studentName = string.split("--")[1];
+        }
+    });
+
+
+    // Create new link Element 
+    var styleLink = document.createElement('link');  
+
+    // set the attributes for link element 
+    styleLink.rel = 'stylesheet';  
+    styleLink.type = 'text/css'; 
+
+    styleLink.href = studentName.toLowerCase() + '.css';  
+
+
+    // Get HTML head element to append  
+    // link element to it  
+    console.log(document.getElementsByTagName('HEAD'));
+    // document.getElementsByTagName('HEAD')[0].appendChild(styleLink);
+}
+
+var studentSwapList = function () {
+
+    if(document.querySelector(".style-swap")) {
+        return;
     }
 
-    var createHeaderLink = function (studentName) {
+    var createStudentElement = function (studentName) {
 
+        var studentElement = document.createElement("li");
+
+        studentElement.classList.add("style-swap__item");
+        studentElement.classList.add("style-swap__item--" + studentName);
+
+        studentElement.textContent = "Styling by: " + studentName;
+
+        return studentElement;
     }
 
     var studentNames = [ 
         'Veronica',
         'Terry',
-        'Katharine',
+        'Kat',
         'Melva',
         'Ameris',
         'Joel',
@@ -25,19 +57,22 @@ var styleSwapper = function () {
         'Chris'
     ];
 
-    studentNames.forEach(function (studentName) {
-        createHeaderLink(studentName);
-        createStudentElement(studentName);
-
-    });
-
-
     var studentList = document.createElement("ul");
     studentList.classList.add("style-swap");
+
+    studentList.addEventListener("click", function (event) {
+        loadStudentStyle(event);
+    });
+
+    studentNames.forEach(function (studentName) {
+        // Create a list item for each student.
+        var studentEl = createStudentElement(studentName);
+
+        studentList.appendChild(studentEl);
+    });
+
+    return studentList;
 }
-
-
-
 
 
 
@@ -46,10 +81,13 @@ var styleSwapper = function () {
 * Take this out for production.
 */ 
 window.onload = function () {
-    var devBtn = document.createElement = "div";
+    var devBtn = document.createElement("div");
     devBtn.textContent = "Load Student List";
+    devBtn.style = "padding: 40px";
+
     devBtn.addEventListener("click", function (e) {
-        styleSwapper();
+        document.body.appendChild(studentSwapList());
     });
-    document.appendChild(devBtn);
+
+    document.body.appendChild(devBtn);
 }
